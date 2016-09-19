f24ad6735519c1e0489e1caae0d1627a1a6ebd85
likaihe <1047561425@qq.com>
2016-09-19 14:01:23 +1200

    test use API to get Json from openweathermap

--- app.js
+++ app.js
@@ -32,7 +32,13 @@ weatherApp.controller("homeController",["$scope","cityService",function($scope,c
 }]);
 
 //forecastController
-weatherApp.controller("forecastController",["$scope","cityService",function($scope,cityService){
+weatherApp.controller("forecastController",["$scope","$resource","cityService",function($scope,$resource,cityService){
    $scope.cityName = cityService.name;
+    
+    $scope.weatherAPI = $resource("http://api.openweathermap.org/data/2.5/forecast?q=London,us&mode=JSON&appid=5a0549aee2a30bfdfa64995d4ba90b3a",{callback:"JSON_CALLBACK"},{ get: {method: "JSONP"}});
    
+    $scope.weatherResult = $scope.weatherAPI.get({});
+    
+    console.log($scope.weatherResult);
+    
 }]);
